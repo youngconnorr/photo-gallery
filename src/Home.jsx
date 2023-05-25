addcf822dda961059014d83723183cb8db29d7fe
connoryoung2 <134354392+connoryoung2@users.noreply.github.com>
2023-05-25 12:07:41 -0600

    edited photo array

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import CircularPhotoArray from './photoArray'
 export default function Home() {
     return (
-        <>
+        <main>
             <CircularPhotoArray />
-            <h2 className='home home-v1'>Creating inspiration  </h2>
-            <h2 className='home home-v2'> Framing fashion   </h2>
-            <h2 className='home home-v3'> &</h2>
-            <h2 className='home home-v4'> Urban photography </h2>
-        </>
+            <h2 className='article home home-v1'>Creating inspiration  </h2>
+            <h2 className='article home home-v2'> Framing fashion   </h2>
+            <h2 className='article home home-v3'>&#38;</h2>
+            <h2 className='article home home-v4'> Urban photography </h2>
+        </main>
     )
 }
--- src/photoArray.jsx
+++ src/photoArray.jsx
@@ -9,6 +9,9 @@ const CircularPhotoArray = () => {
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
+    const showImage = (index) => {
+        setCurrentIndex(index)
+    }
     const goToNextImage = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
     };
@@ -20,11 +23,16 @@ const CircularPhotoArray = () => {
     };
 
     return (
-        <>
-            <button onClick={goToPrevImage} className='photo-buttons bv1'>&lt;</button>
-            <img src={images[currentIndex]} alt="Gallery Image" className='home-img img-fade' />
-            <button onClick={goToNextImage} className='photo-buttons bv2'>&gt;</button>
-        </>
+        <div className='array-container'>
+            <button onClick={goToPrevImage} className='photo-buttons photo-scroll-button-v1'><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z" /></svg></button>
+            {carouselImages.map((image, index) => (
+                <img
+                    src={images[currentIndex]}
+                    alt="Gallery Image"
+                    className={`home-img img-fade ${currentIndex === index ? 'active' : ''}`} />
+            ))}
+            <button onClick={goToNextImage} className='photo-buttons photo-scroll-button-v2'><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" /></svg></button>
+        </div>
     );
 };
 
