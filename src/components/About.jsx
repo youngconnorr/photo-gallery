fd4d23e721e00999a9f01bd483fc286ad0a1c1f0
connoryoung2 <134354392+connoryoung2@users.noreply.github.com>
2023-06-11 14:35:14 -0600

    edited css homepage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
                 A photography portfolio of my work spanning from 2019 to the present day.
             </h2>
 
-            <h2 className='about'>
+            <h2 className='about about-2'>
                 My first personal project from a growing passion for web design. Website is made using CSS, HTML, ReactJS.
             </h2>
         </p>
